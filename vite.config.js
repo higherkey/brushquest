2f07378a419dcd44b2dc8ea4c975734938e03405
Isaac <i.found.the.penni@gmail.com>
2026-07-05 17:49:13 -0500

    fix: Update base path to match GitHub repository name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/timerquest/',
+  base: command === 'serve' ? '/' : '/brushquest/',
   plugins: [
     react(),
     VitePWA({
